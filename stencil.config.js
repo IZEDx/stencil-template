13084b2d7f5f4b995b8df2bbec9475116baf3c35
root <root@ZEDPC.localdomain>
2018-06-05 18:19:34 +0200

    first commit

---
+++ stencil.config.js
@@ -0,0 +1,23 @@
+const sass = require('@stencil/sass');
+
+exports.config = {
+    namespace: "project-td",
+    outputTarget: [
+        {
+            type: 'www',
+            serviceWorker: {
+                swSrc: 'src/sw.js'
+            }
+        }
+    ],
+    globalStyle: 'src/global/style.scss',
+    plugins: [
+        sass()
+    ],
+    copy: []
+};
+
+exports.devServer = {
+    root: 'www',
+    watchGlob: '**/**'
+};
